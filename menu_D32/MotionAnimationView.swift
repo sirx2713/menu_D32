53894082b9ee6670f106b4a76fa6adb9c0246f40
iSAD <bradx2713@gmail.com>
2024-04-07 20:38:03 +0200

    Update MotionAnimationView.swift
    Color Changes

--- menu_D32/MotionAnimationView.swift
+++ menu_D32/MotionAnimationView.swift
@@ -18,7 +18,7 @@ struct MotionAnimationView: View {
         ZStack{
             ForEach(0...randomCircle, id: \.self) {circle in
                 Circle()
-                    .foregroundColor(.gray)
+                    .foregroundColor(.white)
                     .opacity(0.25)
                     .frame(width: randomSize())
                     .position(
@@ -70,3 +70,4 @@ func randomDelay() -> Double{
 #Preview {
     MotionAnimationView()
 }
+
